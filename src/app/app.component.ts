8d0807a5da3d185f130ac52b5c52aa354c41ca38
aigenseer <viktor.aigenseer@web.de>
2022-03-31 20:59:37 +0200

    LFD-51 [web] cactus-easteregg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EasterEgg} from "./lib/EasterEgg";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{}
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    if(environment.production){
+      EasterEgg.print()
+    }
+  }
+
+
+}
