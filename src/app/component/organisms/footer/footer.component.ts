65c858c9781480331a4343931e26ff862a5f66df
aigenseer <viktor.aigenseer@web.de>
2022-04-09 21:42:37 +0200

    LFD-116 [web] refactor footer

--- src/app/component/organisms/footer/footer.component.ts
+++ src/app/component/organisms/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,14 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  visible: boolean = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(value => {
+      this.visible = !(value.hasOwnProperty("hidefooter") && value.hidefooter == 1);
+    });
   }
 
 }
