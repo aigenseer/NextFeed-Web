0a113938622fe6a62e4e72668dd6d4252a6076de
Lukas <lukas.hoernle@web.de>
2022-04-27 11:28:30 +0200

    fixed errors caused by styling option generated at https://qr-code-styling.com/

--- src/app/component/organisms/share-session-code-dialog/share-session-code-dialog.component.ts
+++ src/app/component/organisms/share-session-code-dialog/share-session-code-dialog.component.ts
@@ -25,12 +25,12 @@ export class ShareSessionCodeDialogComponent implements OnChanges{
     "dotsOptions":{"type":"rounded","color":"#1a6b41","gradient":{"type":"radial","rotation":0,"colorStops":[{"offset":0,"color":"#009688"},{"offset":1,"color":"#004942"}]}},
     "backgroundOptions":{"color":"#ffffff","gradient":undefined},
     "image":"src/assets/lecturefeed_logo.svg",
-    "dotsOptionsHelper":{"colorType":{"single":true,"gradient":false},"gradient":{"linear":true,"radial":false,"color1":"#6a1a4c","color2":"#6a1a4c","rotation":"0"}},
+    
     "cornersSquareOptions":{"type":"extra-rounded","color":"#006a61","gradient":undefined},
-    "cornersSquareOptionsHelper":{"colorType":{"single":true,"gradient":false},"gradient":{"linear":true,"radial":false,"color1":"#000000","color2":"#000000","rotation":"0"}},
+    
     "cornersDotOptions":{"type":undefined,"color":"#1b4f46"},
-    "cornersDotOptionsHelper":{"colorType":{"single":true,"gradient":false},"gradient":{"linear":true,"radial":false,"color1":"#000000","color2":"#000000","rotation":"0"}},
-    "backgroundOptionsHelper":{"colorType":{"single":true,"gradient":false},"gradient":{"linear":true,"radial":false,"color1":"#ffffff","color2":"#ffffff","rotation":"0"}}
+    
+    
 }
 
   constructor(
